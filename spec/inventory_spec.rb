b8d8d8494e016972b09daca3183508775ded8cb8
Dominic Brown <yeahdom@gmail.com>
2017-05-12 15:28:58 -0700

    refactor tests to work properly with scope method

--- spec/inventory_spec.rb
+++ spec/inventory_spec.rb
@@ -1,17 +1,18 @@
-# require "spec_helper"
-#
-# describe Inventory do
-#
-#   it { should belong_to(:store) }
-#   it { should belong_to(:brand) }
-#
-#   it ("finds a single RECORD from the INVENTORIES table") do
-#     store1 = Store.new({:name => "footlocker"})
-#     store1.save
-#     brand1 = Brand.new({:name => "adidas", :price => "59.99", :store_ids => [store1.id]})
-#     brand1.save
-#     found_inv_record = Inventory.all.find_inv_record(store1.id, brand1.id)
-#     expect(found_inv_record[0].brand_id).to(eq(brand1.id))
-#   end
-#
-# end
+require "spec_helper"
+
+describe Inventory do
+
+  it { should belong_to(:store) }
+  it { should belong_to(:brand) }
+
+  it ("finds a single RECORD from the INVENTORIES table") do
+    store1 = Store.new({:name => "footlocker"})
+    store1.save
+    brand1 = Brand.new({:name => "adidas", :price => 59.99})
+    brand1.save
+    Inventory.create({:store_id => store1.id, :brand_id => brand1.id})
+    found_inv_record = Inventory.all.find_inv_record(store1.id, brand1.id)
+    expect(found_inv_record[0].brand_id).to(eq(brand1.id))
+  end
+
+end
