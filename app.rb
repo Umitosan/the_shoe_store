a887ec7f99880d210fc21d22cedb5387e01aefcf
Dominic Brown <yeahdom@gmail.com>
2017-05-12 16:36:26 -0700

    add a store home page path which displays when a a store link is clicked from the main page

--- app.rb
+++ app.rb
@@ -35,6 +35,8 @@ post "/create_brand" do
 end
 
 get "/stores/:store_id" do
+  store_id = params.fetch("store_id").to_i
+  @found_store = Store.find_by(id: store_id)
   erb :store_home
 end
 
--- spec/shoe_store_integration_spec.rb
+++ spec/shoe_store_integration_spec.rb
@@ -8,7 +8,6 @@ describe("the home page", {:type => :feature}) do
     click_button("Save Store")
     expect(page).to(have_content("Footlocker"))
   end
-
   it("adds a new store to the list") do
     visit("/")
     fill_in("brand_name", :with => "Nike")
@@ -16,4 +15,16 @@ describe("the home page", {:type => :feature}) do
     expect(page).to(have_content("Nike"))
   end
 
+end
+
+describe("the selected store page ", {:type => :feature}) do
+
+  it("displays the current store name on the page") do
+    visit("/")
+    fill_in("store_name", :with => "Footlocker")
+    click_button("Save Store")
+    click_link("Footlocker")
+    expect(page).to(have_content("Footlocker"))
+  end
+  
 end
