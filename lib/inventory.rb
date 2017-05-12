6f518df2a2cbc20ec9cd437c60cd4487c5cc307e
Dominic Brown <yeahdom@gmail.com>
2017-05-12 11:30:07 -0700

    add scope to inventory class to find a single store-brand association

--- lib/inventory.rb
+++ lib/inventory.rb
@@ -2,4 +2,9 @@ class Inventory < ActiveRecord::Base
   belongs_to(:store)
   belongs_to(:brand)
 
+  # this scope method helps us return a single inventory entry between a store and brands
+  scope(:find_inv_record, -> (st_id, br_id) do
+    where({:store_id => st_id, :brand_id => br_id})
+  end)
+  
 end
--- spec/inventory_spec.rb
+++ spec/inventory_spec.rb
@@ -5,4 +5,11 @@ describe Inventory do
   it { should belong_to(:store) }
   it { should belong_to(:brand) }
 
+  it ("finds a single RECORD from the INVENTORIES table") do
+    brand1 = Brand.create({:name => "adidas", :price => "59.99"})
+    store1 = Store.create({:name => "footlocker", :brand_ids => [brand1.id]})
+    found_inv_record = Inventory.all.find_inv_record(store1.id, brand1.id)
+    expect(found_inv_record[0].brand_id).to(eq(brand1.id))
+  end
+
 end
